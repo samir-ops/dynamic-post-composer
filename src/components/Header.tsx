dcd9d2fe2ae04eb4529d365f444a17fddcaa86c8
samir-ops <samirsharma1336@gmail.com>
2026-07-21 11:59:38 +0530

    Create Header.tsx

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import "../styles/Header.css";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1>Dynamic Post Composer</h1>
+
+      <p>
+        Compose one post and instantly validate it for multiple social media
+        platforms.
+      </p>
+    </header>
+  );
+};
+
+export default Header;
