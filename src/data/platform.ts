f3731607328ecea8808b78fe34fe69e13f4e24bd
samir-ops <samirsharma1336@gmail.com>
2026-07-21 12:00:52 +0530

    Create platform.ts

---
+++ src/data/platform.ts
@@ -0,0 +1,24 @@
+import { Platform } from "../types/Platform";
+
+export const platforms: Platform[] = [
+  {
+    id: "twitter",
+    name: "Twitter (X)",
+    limit: 280,
+  },
+  {
+    id: "linkedin",
+    name: "LinkedIn",
+    limit: 3000,
+  },
+  {
+    id: "instagram",
+    name: "Instagram",
+    limit: 2200,
+  },
+  {
+    id: "facebook",
+    name: "Facebook",
+    limit: 63206,
+  },
+];
